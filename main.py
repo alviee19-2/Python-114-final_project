0421b6830ebf9035cabae76804a594a722c5cbd3
alviee19 <alvis.wu.2005@gmail.com>
2025-05-25 02:35:38 +0800

    新增database來存抓下來的東西

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
+import pandas as pd
+import datetime as dt
 from dotenv import load_dotenv
 from FinMind.data import DataLoader
 
 #login
 load_dotenv()
 
-API_TOKEN = os.getenv("FINMIND_TOKEN")
+#API_TOKEN = os.getenv("FINMIND_TOKEN")
 USER_NAME = os.getenv("FINMIND_USER")
 PASSWORD = os.getenv("FINMIND_PASSWORD")
 
@@ -14,4 +16,10 @@ data_loader.login(user_id= USER_NAME,password= PASSWORD)
 
 df = data_loader.taiwan_stock_info()
 
-print(df)
+now = dt.datetime.now()
+timestamp = now.strftime("%Y%m%d_%H%M")
+
+filepath = 'db/stockinfo/'
+filename = 'stockinfo' + timestamp + '.csv'
+df.to_csv(filepath + filename, index = False)
+
